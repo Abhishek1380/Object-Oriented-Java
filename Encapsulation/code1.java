5f5d06010b1771dd921d1a8f2742b196bc4830c8
Abhishek1380 <undeabhishek707@gmail.com>
2025-08-01 22:57:32 +0530

    Oops repository initiated

---
+++ Encapsulation/code1.java
@@ -0,0 +1,12 @@
+
+class Student{
+    private String name;
+
+    function void setName(String str){
+        this.name = str;
+    }
+
+    function String getName(){
+        return name;
+    }
+}
